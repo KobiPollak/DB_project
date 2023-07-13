1d8005466a92cb13c0a6798237aebfceef2dde87
Kobi Pollak <kobipollak@gmail.com>
2023-07-13 10:19:47 +0300

    adding manage between two pages using navbar

--- Client/db_client/src/App.js
+++ Client/db_client/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState } from "react";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect as Redirect,
+  Navigate,
+} from "react-router-dom";
+
+import HomePage from "./components/homepage";
+import Manager from "./components/manager";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "home":
+        return <HomePage />;
+      case "manager":
+        return <Manager />;
+      default:
+        return <HomePage />;
+    }
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <div className="nav" onClick={() => handlePageChange("home")}>
+              Home
+            </div>
+          </li>
+          <li>
+            <div className="nav" onClick={() => handlePageChange("manager")}>
+              Manager
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <div>{renderPage()}</div>
     </div>
   );
 }
